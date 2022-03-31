20b06b18840b496d07806d2ec3b5f6e9662c5d68
Kei47 <trankhanh040147@gmail.com>
2022-03-31 23:01:54 +0700

    update 1

--- wftest2/wftest2/Form1.Designer.cs
+++ wftest2/wftest2/Form1.Designer.cs
@@ -30,6 +30,7 @@ namespace wftest2
         private void InitializeComponent()
         {
             this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // listBox1
@@ -41,11 +42,21 @@ namespace wftest2
             this.listBox1.Size = new System.Drawing.Size(463, 284);
             this.listBox1.TabIndex = 0;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(35, 91);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.listBox1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -57,6 +68,7 @@ namespace wftest2
         #endregion
 
         private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
 

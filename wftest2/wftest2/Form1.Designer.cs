3b140f61b61ee30d64554f6d69a7bc3aa3c36658
Kei47 <trankhanh040147@gmail.com>
2022-03-31 23:05:37 +0700

    update listbox

--- wftest2/wftest2/Form1.Designer.cs
+++ wftest2/wftest2/Form1.Designer.cs
@@ -29,35 +29,24 @@ namespace wftest2
         /// </summary>
         private void InitializeComponent()
         {
-            this.listBox1 = new System.Windows.Forms.ListBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.Fuckyou = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
-            // listBox1
+            // Fuckyou
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 20;
-            this.listBox1.Location = new System.Drawing.Point(202, 56);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(463, 284);
-            this.listBox1.TabIndex = 0;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(35, 91);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(94, 29);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.Fuckyou.FormattingEnabled = true;
+            this.Fuckyou.ItemHeight = 20;
+            this.Fuckyou.Location = new System.Drawing.Point(202, 56);
+            this.Fuckyou.Name = "Fuckyou";
+            this.Fuckyou.Size = new System.Drawing.Size(463, 284);
+            this.Fuckyou.TabIndex = 0;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.Fuckyou);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -67,8 +56,7 @@ namespace wftest2
 
         #endregion
 
-        private System.Windows.Forms.ListBox listBox1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox Fuckyou;
     }
 }
 

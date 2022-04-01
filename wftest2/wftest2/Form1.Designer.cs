e2b8f92c6fb0f03d457160a681bfa09b21489269
Kei47 <trankhanh040147@gmail.com>
2022-04-01 20:30:16 +0700

    upd wftest2 2

--- wftest2/wftest2/Form1.Designer.cs
+++ wftest2/wftest2/Form1.Designer.cs
@@ -31,35 +31,45 @@ namespace wftest2
         {
             this.Fuckyou = new System.Windows.Forms.ListBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // Fuckyou
             // 
             this.Fuckyou.FormattingEnabled = true;
-            this.Fuckyou.ItemHeight = 15;
-            this.Fuckyou.Location = new System.Drawing.Point(177, 42);
-            this.Fuckyou.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Fuckyou.ItemHeight = 20;
+            this.Fuckyou.Location = new System.Drawing.Point(202, 56);
             this.Fuckyou.Name = "Fuckyou";
-            this.Fuckyou.Size = new System.Drawing.Size(406, 214);
+            this.Fuckyou.Size = new System.Drawing.Size(463, 284);
             this.Fuckyou.TabIndex = 0;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(342, 124);
+            this.button1.Location = new System.Drawing.Point(391, 165);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(86, 31);
             this.button1.TabIndex = 1;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(55, 68);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 29);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(700, 338);
+            this.ClientSize = new System.Drawing.Size(800, 451);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.Fuckyou);
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -71,6 +81,7 @@ namespace wftest2
 
         private System.Windows.Forms.ListBox Fuckyou;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
